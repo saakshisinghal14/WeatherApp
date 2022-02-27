0dc5e7f59334ca59be2efe85f6b01a8a1f53d331
saakshi <saakshisinghal14@gmail.com>
2022-02-27 19:19:03 +0530

    Updates

---
+++ precache-manifest.72648337ebf0610b06cbc5c8d49f2465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97df706750db8cc59a29",
+    "url": "/weatherApp/static/css/main.6da7cb2a.chunk.css"
+  },
+  {
+    "revision": "97df706750db8cc59a29",
+    "url": "/weatherApp/static/js/main.97df7067.chunk.js"
+  },
+  {
+    "revision": "517c2aed3152073fba79",
+    "url": "/weatherApp/static/js/1.517c2aed.chunk.js"
+  },
+  {
+    "revision": "0ff3e20d12400699242a",
+    "url": "/weatherApp/static/js/runtime~main.0ff3e20d.js"
+  },
+  {
+    "revision": "8e3c872b45e8b53cd7cf3f1710884e15",
+    "url": "/weatherApp/index.html"
+  }
+];
